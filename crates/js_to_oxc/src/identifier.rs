a1e73e4083ba79cad6dc509dee20623d65f12e74
_Kerman <kermanx@qq.com>
2024-07-27 23:35:08 +0800

    feat: support more statements

--- crates/js_to_oxc/src/identifier.rs
+++ crates/js_to_oxc/src/identifier.rs
@@ -1,5 +1,5 @@
 use crate::JsToOxc;
-use oxc::ast::ast::BindingIdentifier;
+use oxc::ast::ast::{BindingIdentifier, LabelIdentifier};
 use proc_macro2::TokenStream;
 use quote::quote;
 
@@ -10,6 +10,15 @@ impl JsToOxc {
     let name = identifier.name.as_str();
     quote! {
       #ast_builder.binding_identifier(#span, #name)
+    }
+  }
+
+  pub(crate) fn gen_label_identifier(&self, identifier: &LabelIdentifier) -> TokenStream {
+    let ast_builder = &self.ast_builder;
+    let span = &self.span;
+    let name = identifier.name.as_str();
+    quote! {
+      #ast_builder.label_identifier(#span, #name)
     }
   }
 }
--- crates/js_to_oxc/src/statement.rs
+++ crates/js_to_oxc/src/statement.rs
@@ -1,22 +1,65 @@
 use crate::{utils::unimplemented, JsToOxc};
 use oxc::ast::ast::Statement;
 use proc_macro2::TokenStream;
-use quote::TokenStreamExt;
+use quote::quote;
 
 impl JsToOxc {
-  pub fn gen_statement(&self, node: &Statement) -> TokenStream {
-    let mut tokens = TokenStream::new();
+  pub(crate) fn gen_statement(&self, node: &Statement) -> TokenStream {
+    let ast_builder = &self.ast_builder;
+    let span = &self.span;
     match node {
-      Statement::BlockStatement(block) => {
-        for stmt in &block.body {
-          tokens.append_all(self.gen_statement(stmt));
+      Statement::BlockStatement(node) => {
+        let body = self.gen_vec(&node.body, |statement| self.gen_statement(statement));
+        quote! {
+          #ast_builder.block_statement(#span, #body)
         }
       }
-      Statement::ExpressionStatement(expr) => {
-        tokens.append_all(self.gen_expression(&expr.expression));
+      Statement::BreakStatement(node) => {
+        let label = self.gen_option(&node.label, |label| self.gen_label_identifier(label));
+        quote! {
+          #ast_builder.break_statement(#span, #label)
+        }
       }
-      _ => tokens.append_all(unimplemented()),
+      Statement::ContinueStatement(node) => {
+        let label = self.gen_option(&node.label, |label| self.gen_label_identifier(label));
+        quote! {
+          #ast_builder.continue_statement(#span, #label)
+        }
+      }
+      Statement::DebuggerStatement(_) => {
+        quote! {
+          #ast_builder.debugger_statement(#span)
+        }
+      }
+      Statement::DoWhileStatement(_) => unimplemented(),
+      Statement::EmptyStatement(_) => {
+        quote! {
+          #ast_builder.empty_statement(#span)
+        }
+      }
+      Statement::ExpressionStatement(node) => {
+        let expression = self.gen_expression(&node.expression);
+        quote! {
+          #ast_builder.statement_expression(#span, #expression)
+        }
+      }
+      Statement::ForInStatement(_) => unimplemented(),
+      Statement::ForOfStatement(_) => unimplemented(),
+      Statement::ForStatement(_) => unimplemented(),
+      Statement::IfStatement(_) => unimplemented(),
+      Statement::LabeledStatement(_) => unimplemented(),
+      Statement::ReturnStatement(node) => {
+        let argument = self.gen_option(&node.argument, |argument| self.gen_expression(argument));
+        quote! {
+          #ast_builder.statement_return(#span, #argument)
+        }
+      }
+      Statement::SwitchStatement(_) => unimplemented(),
+      Statement::ThrowStatement(_) => unimplemented(),
+      Statement::TryStatement(_) => unimplemented(),
+      Statement::WhileStatement(_) => unimplemented(),
+      Statement::WithStatement(_) => unimplemented(),
+      _ => unimplemented(),
     }
-    tokens
   }
 }
