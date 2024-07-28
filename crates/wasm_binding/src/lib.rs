add8480c5cd55611ed243b00c856eca7cd6ff552
_Kerman <kermanx@qq.com>
2024-07-28 14:53:25 +0800

    feat: update ui

--- crates/wasm_binding/src/lib.rs
+++ crates/wasm_binding/src/lib.rs
@@ -4,11 +4,21 @@ use quote::quote;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn run(source: &str) -> String {
+pub fn generate_expression(source: &str) -> String {
   let allocator = Allocator::default();
   let source_type = SourceType::default();
   let expression = Parser::new(&allocator, source, source_type).parse_expression().unwrap();
   let gen = JsToOxc { ast_builder: quote! { self.ast_builder }, span: quote! { SPAN } };
   let tokens = gen.gen_expression(&expression);
+  tokens.to_string()
+}
+
+#[wasm_bindgen]
+pub fn generate_program(source: &str) -> String {
+  let allocator = Allocator::default();
+  let source_type = SourceType::default();
+  let program = Parser::new(&allocator, source, source_type).parse().program;
+  let gen = JsToOxc { ast_builder: quote! { self.ast_builder }, span: quote! { SPAN } };
+  let tokens = gen.gen_program(&program);
   tokens.to_string()
 }
--- packages/ui/src/App.vue
+++ packages/ui/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { debouncedWatch, useLocalStorage } from '@vueuse/core'
-import { run as jsToOxc } from 'js_to_oxc_wasm'
+import { generate_expression, generate_program } from 'js_to_oxc_wasm'
 import { ref, shallowRef } from 'vue'
 import Editor from './Editor.vue'
 import { formatRust } from './format'
@@ -8,58 +8,73 @@ import { formatRust } from './format'
 const js = ref('console.log("Hello, World!")')
 const autoRun = useLocalStorage('js_to_oxc:autoRun', true)
 const shouldFormat = useLocalStorage('js_to_oxc:shouldFormat', true)
+const programMode = useLocalStorage('js_to_oxc:programMode', false)
 
 const formatted = ref('')
-const loading = ref(true)
+const formatting = ref(false)
 const error = ref('')
 const controller = shallowRef<AbortController>()
 
 async function run() {
   const unformatted = `impl _ {
     fn create_ast(&self) {
-      ${jsToOxc(js.value)}
+      ${(programMode.value ? generate_program : generate_expression)(js.value)}
     }
   }`
   controller.value?.abort()
   const { signal } = controller.value = new AbortController()
   try {
     error.value = ''
-    loading.value = true
-    formatted.value = shouldFormat.value ? await formatRust(unformatted, signal) : unformatted
-    loading.value = false
+    formatted.value = unformatted
+    if (shouldFormat.value) {
+      formatting.value = true
+      formatRust(unformatted, signal).then((result) => {
+        if (!signal.aborted) {
+          formatted.value = result
+          formatting.value = false
+        }
+      }).catch((err) => {
+        if (!signal.aborted) {
+          error.value = String(err)
+          formatted.value = unformatted
+          formatting.value = false
+        }
+      })
+    }
   }
   catch (err: any) {
     if (err instanceof DOMException && err.name === 'AbortError')
       return
     error.value = String(err)
     formatted.value = unformatted
-    loading.value = false
+    formatting.value = false
   }
 }
 
-debouncedWatch(() => [autoRun.value && js.value, shouldFormat.value], ([source]) => {
-  if (source)
-    run()
-}, { immediate: true, debounce: 300 })
+debouncedWatch(
+  () => autoRun.value && [js.value, shouldFormat.value, programMode.value],
+  shouldRun => shouldRun && run(),
+  { immediate: true, debounce: 300 },
+)
 </script>
 
 <template>
-  <div p-4 fixed inset-0 flex flex-col>
-    <div flex>
-      <h1 text-3xl font-bold pb-4>
+  <div py-4 fixed inset-0 flex flex-col>
+    <div px-4 flex>
+      <h1 text-3xl font-bold pb-4 select-none>
         JS to Oxc
       </h1>
       <div flex-grow />
-      <div flex flex-col h-0>
-        <div>
+      <div flex flex-col h-0 z-10>
+        <div select-none>
           Options
         </div>
         <div flex-grow text-sm>
-          <label flex align-center gap-1>
+          <label flex align-center gap-1 select-none>
             <input v-model="autoRun" type="checkbox">
             Auto Run
           </label>
-          <label flex align-center gap-1>
+          <label flex align-center gap-1 select-none>
             <input v-model="shouldFormat" type="checkbox">
             Format Rust
           </label>
@@ -69,12 +84,16 @@ debouncedWatch(() => [autoRun.value && js.value, shouldFormat.value], ([source])
     <div flex-grow grid grid-cols-2 gap-6>
       <div flex flex-col>
         <div flex items-center>
-          <h2 text-2xl pb-2 pl-2>
+          <h2 text-2xl pb-2 pl-4 select-none>
             Input JS
           </h2>
           <div flex-grow />
+          <label flex align-center gap-1>
+            <input v-model="programMode" type="checkbox">
+            Program Mode
+          </label>
           <div>
-            <button bg-gray-400 bg-op-40 px-3 rounded hover:bg-op-50 @click="run">
+            <button bg-gray-400 bg-op-40 px-3 rounded hover:bg-op-50 select-none @click="run">
               Run
             </button>
           </div>
@@ -82,12 +101,12 @@ debouncedWatch(() => [autoRun.value && js.value, shouldFormat.value], ([source])
         <Editor v-model="js" lang="javascript" class="w-full h-full" />
       </div>
       <div flex flex-col>
-        <h2 text-2xl pb-2 pl-2>
+        <h2 text-2xl pb-2 pl-4 select-none>
           Output RS
         </h2>
         <div flex-grow relative>
           <Editor v-model="formatted" lang="rust" readonly class="w-full h-full" />
-          <div z-20 absolute w-full top-0 children:p-2 children:px-3 children:b-2 children:rounded>
+          <div z-20 absolute left-1 right-1 top-0 children:p-2 children:px-3 children:b-2 children:rounded>
             <div v-if="error" text-red-200 bg-red-900 bg-op-80 b-red-500>
               <h3 text-lg pb-1>
                 Error
@@ -96,7 +115,7 @@ debouncedWatch(() => [autoRun.value && js.value, shouldFormat.value], ([source])
                 {{ error }}
               </div>
             </div>
-            <div v-if="loading" text-gray-300 bg-gray-900 bg-op-80 b-gray-500>
+            <div v-if="formatting" text-gray-300 bg-gray-900 bg-op-80 b-gray-500>
               <h3 text-lg pb-1>
                 Running
               </h3>
@@ -104,7 +123,7 @@ debouncedWatch(() => [autoRun.value && js.value, shouldFormat.value], ([source])
                 Formatting Rust code...
               </div>
             </div>
-            <button v-if="error || loading" absolute right-4 top-1 w-4 h-4 b-none i-carbon-close @click="error = ''; loading = false" />
+            <button v-if="error || formatting" absolute right-3 top-3 w-6 h-6 b-none i-carbon-close @click="error = ''; formatting = false" />
           </div>
         </div>
       </div>
